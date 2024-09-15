41f5bda4fbda83d5de12feffd8e5e1020302fb4d
Aleksey Buzulenkov <begunko@gmail.com>
2024-09-15 05:42:33 +0300

    h3

---
+++ 6.classes/data.py
@@ -0,0 +1,276 @@
+students = [
+    {
+        "name": "Григорий",
+        "surname": "Печерин",
+        "gender": "м",
+        "finished_courses": [
+            "Python-разработчик с нуля",
+            "Backend-зразработчик на Django",
+        ],
+        "courses_in_progress": ["Системный администратор"],
+        "grades": {"Системный администратор": [10, 10, 8, 9, 7]},
+    },
+    {
+        "name": "Андрей",
+        "surname": "Болконский",
+        "gender": "м",
+        "finished_courses": ["Backend-зразработчик на Django"],
+        "courses_in_progress": ["Frontend-разработчик"],
+        "grades": {"Frontend-разработчик": [9, 8, 8, 10, 7]},
+    },
+    {
+        "name": "Александр",
+        "surname": "Чацкий",
+        "gender": "м",
+        "finished_courses": [],
+        "courses_in_progress": [
+            "Веб-разработчик с нуля: профессия с выбором специальностей"
+        ],
+        "grades": {"Веб-разработчик с нуля: профессия с выбором специальностей": []},
+    },
+    {
+        "name": "Остап",
+        "surname": "Бендер",
+        "gender": "м",
+        "finished_courses": ["SQL и получение данных", "Python-разработчик с нуля"],
+        "courses_in_progress": ["Fullstack-разработчик на Pyhton"],
+        "grades": {"Fullstack-разработчик на Pyhton": [10, 8, 8, 10, 7, 10, 9, 8, 8]},
+    },
+    {
+        "name": "Эраст",
+        "surname": "Фондорин",
+        "gender": "м",
+        "finished_courses": [
+            "SQL и получение данных",
+            "Python-разработчик: расширенный курс",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Алексей",
+        "surname": "Вронский",
+        "gender": "м",
+        "finished_courses": [
+            "Python-разработчик с нуля",
+            "Backend-зразработчик на Django",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Владимир",
+        "surname": "Дубровский",
+        "gender": "м",
+        "finished_courses": ["Python-разработчик с нуля"],
+        "courses_in_progress": ["SQL и получение данных"],
+        "grades": {"SQL и получение данных": [10, 7]},
+    },
+    {
+        "name": "Андрей",
+        "surname": "Штольц",
+        "gender": "м",
+        "finished_courses": ["Python-разработчик: расширенный курс"],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Николай",
+        "surname": "Ставригин",
+        "gender": "м",
+        "finished_courses": [
+            "Python-разработчик с нуля",
+            "Fullstack-разработчик на Pyhton",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Пётр",
+        "surname": "Верховенский",
+        "gender": "м",
+        "finished_courses": ["Backend-зразработчик на Django"],
+        "courses_in_progress": ["Fullstack-разработчик на Pyhton"],
+        "grades": {"Fullstack-разработчик на Pyhton": [8, 9, 6]},
+    },
+    {
+        "name": "Агафья",
+        "surname": "Пшеницына",
+        "gender": "ж",
+        "finished_courses": ["Fullstack-разработчик на Pyhton"],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Соня",
+        "surname": "Мармеладова",
+        "gender": "ж",
+        "finished_courses": [
+            "SQL и получение данных",
+            "Fullstack-разработчик на Pyhton",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Аксинья",
+        "surname": "Астахова",
+        "gender": "ж",
+        "finished_courses": ["SQL и получение данных", "Python-разработчик с нуля"],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Татьяна",
+        "surname": "Ларина",
+        "gender": "ж",
+        "finished_courses": [
+            "Python-разработчик с нуля",
+            "Backend-зразработчик на Django",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Анна",
+        "surname": "Каренина",
+        "gender": "ж",
+        "finished_courses": ["Python-разработчик с нуля"],
+        "courses_in_progress": ["Python-разработчик: расширенный курс"],
+        "grades": {"Python-разработчик: расширенный курс": [10, 8, 10, 7]},
+    },
+    {
+        "name": "Наталья",
+        "surname": "Ростова",
+        "gender": "ж",
+        "finished_courses": ["Python-разработчик: расширенный курс"],
+        "courses_in_progress": ["SQL и получение данных"],
+        "grades": {"SQL и получение данных": [10, 7]},
+    },
+    {
+        "name": "Ирина",
+        "surname": "Прозорова",
+        "gender": "ж",
+        "finished_courses": ["SQL и получение данных", "Python-разработчик с нуля"],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Лиза",
+        "surname": "Калитина",
+        "gender": "ж",
+        "finished_courses": [
+            "Python-разработчик с нуля",
+            "Backend-зразработчик на Django",
+        ],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Ольга",
+        "surname": "Семенова",
+        "gender": "ж",
+        "finished_courses": ["Python-разработчик с нуля"],
+        "courses_in_progress": [],
+    },
+    {
+        "name": "Анна",
+        "surname": "Одинцова",
+        "gender": "ж",
+        "finished_courses": [
+            "Python-разработчик: расширенный курс",
+            "Fullstack-разработчик на Pyhton",
+        ],
+        "courses_in_progress": [],
+    },
+]
+
+lecturers = [
+    {
+        "name": "Фёдор",
+        "surname": "Достоевский",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик с нуля",
+            "Backend-зразработчик на Django",
+            "Python-разработчик: расширенный курс",
+        ],
+    },
+    {
+        "name": "Михаил",
+        "surname": "Лермонтов",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик с нуля",
+            "Fullstack-разработчик на Pyhton",
+        ],
+    },
+    {
+        "name": "Михаил",
+        "surname": "Булгаков",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик: расширенный курс",
+            "Backend-зразработчик на Django",
+            "SQL и получение данных",
+        ],
+    },
+    {
+        "name": "Иван",
+        "surname": "Тургеньев",
+        "gender": "м",
+        "courses_attached": [
+            "Frontend-разработчик",
+            "Веб-разработчик с нуля: профессия с выбором специальностей",
+        ],
+    },
+    {
+        "name": "Николай",
+        "surname": "Карамзин",
+        "gender": "м",
+        "courses_attached": [
+            "SQL и получение данных",
+            "Backend-зразработчик на Django",
+        ],
+    },
+]
+
+reviewers = [
+    {
+        "name": "Михаил",
+        "surname": "Ломоносов",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик с нуля",
+            "Python-разработчик: расширенный курс",
+        ],
+    },
+    {
+        "name": "Николай",
+        "surname": "Лобачевский",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик с нуля",
+            "Fullstack-разработчик на Pyhton",
+            "Frontend-разработчик",
+        ],
+    },
+    {
+        "name": "Александр",
+        "surname": "Попов",
+        "gender": "м",
+        "courses_attached": [
+            "Python-разработчик: расширенный курс",
+            "Backend-зразработчик на Django",
+        ],
+    },
+    {
+        "name": "Сергей",
+        "surname": "Кораблёв",
+        "gender": "м",
+        "courses_attached": [
+            "Frontend-разработчик",
+            "Веб-разработчик с нуля: профессия с выбором специальностей",
+        ],
+    },
+    {
+        "name": "Дмитрий",
+        "surname": "Менделеев",
+        "gender": "м",
+        "courses_attached": [
+            "SQL и получение данных",
+            "Backend-зразработчик на Django",
+            "Системный администратор",
+        ],
+    },
+]
